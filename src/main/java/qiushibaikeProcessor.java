2efd853c06051abc4382a143b195259e07873554
TTTTT <fengyang.avery@gmail.com>
2017-12-26 22:47:39 +0800

    17/12/26

---
+++ src/main/java/qiushibaikeProcessor.java
@@ -0,0 +1,33 @@
+import org.apache.commons.logging.impl.Log4JLogger;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * Created by TTTTT on 2017/12/26 0026.
+ */
+public class qiushibaikeProcessor implements PageProcessor {
+
+    static Log4JLogger l = new Log4JLogger("log4j.properties");
+
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000).setTimeOut(10000).addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+
+    public static void main(String[] args) {
+        Spider.create(new qiushibaikeProcessor()).addUrl("https://www.qiushibaike.com/hot/").thread(5).run();
+    }
+
+    @Override
+    public void process(Page page) {
+        l.info(page.getHtml().xpath("//a[@class=\"contentHerf\"]//div[@class=\"content\"]//").toString());
+        System.out.println("抓取的内容：" + page.getHtml().xpath("//a[@class=\"contentHerf\"]//div[@class=\"content\"]//").toString());
+        page.addTargetRequests(page.getHtml().xpath("//*[@id=\"content-left\"]/ul/li/a/@href").all());
+        page.putField("content", page.getHtml().xpath("//*[@class=\"contentHerf\"]/div/@span").toString());
+
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+}
